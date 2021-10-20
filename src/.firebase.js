0cd66be286a5abfa13aadd07541556e0e93973f3
Ankit Tripathi <ankittripathi1456@email.com>
2021-10-20 17:49:46 +0530

    made some changes

---
+++ src/.firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJlNcnLwOMOmaTVkxNSM5s4TzX-qpWtII",
+  authDomain: "intrigd-7cb69.firebaseapp.com",
+  projectId: "intrigd-7cb69",
+  storageBucket: "intrigd-7cb69.appspot.com",
+  messagingSenderId: "981316495561",
+  appId: "1:981316495561:web:33744058a6cbde79cc8518",
+  measurementId: "G-VD3VNLP3C5",
+};
